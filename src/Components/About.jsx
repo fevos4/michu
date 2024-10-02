72ba0a727db065f37a8d85464754c74ff5564a62
fevos4 <fevatesfish@gmail.com>
2024-10-02 14:44:09 +0300

    Updated again

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const About = () => {
     return (
-        <div className="w-full flex flex-col justify-between px-6 md:p-24" id="about">
-            <div className="grid md:grid-cols-2 gap-4 md:gap-16"> 
+        <div className="w-full flex flex-col items-center px-6 md:p-24" id="about">
+            <div className="grid md:grid-cols-2 gap-4 md:gap-16 max-w-5xl w-full">
                 <div className="order-2 md:order-1 justify-center hidden md:flex">
-                    <img src="/assets/about.png" alt="Picture" className="h-60 md:h-80 md:ml-20 object-contain" />
+                    <img src="/assets/about.png" alt="Picture" className="h-60 md:h-80 object-contain" />
                 </div>
                 <div className="flex flex-col justify-center md:items-start w-full px-2 py-8 order-1 md:order-2">
-                    <p className="font-bold text-2xl text-left">Why Us?</p>
-                    <p className="py-3 md:mb-5 font-regi text-left">
+                    <p className="font-bold text-2xl text-center md:text-left">Why Us?</p>
+                    <p className="py-3 md:mb-5 font-regi text-center md:text-left">
                         At <span className="text-[#00A551]">MichuTech IT Solutions PLC</span>, we serve as your bridge to a future enhanced by digital solutions and cutting-edge application development.
                     </p>
-                    <p className="font-bold text-2xl text-left">Our Offerings:</p>
-                    <ul className="list-disc pl-5 text-left">
+                    <p className="font-bold text-2xl text-center md:text-left">Our Offerings:</p>
+                    <ul className="list-disc pl-5 text-center md:text-left">
                         <li className="font-regi">Mobile Application</li>
                         <li className="font-regi">Web Application</li>
                         <li className="font-regi">AI Expertise</li>
